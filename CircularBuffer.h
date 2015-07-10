c3aeb1fbffa4c7aab192042e48854514470f5e62
Amauri <amaurial@uol.com.br>
2015-07-11 09:54:29 +1200

    circular buffer adjustments.

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -2,7 +2,7 @@
 #define CIRCULARBUFFER_H
 
 #define _msgSize 15 /**<Size of the message stored in the buffer. 1 byte for the message size.1 byte for RTR, 4 bytes for header. 8 bytes to max message */
-#define CIRCULARBUFFER_MSGS 10 /**<Number of messages of size _msgSize to be stored.*/
+#define CIRCULARBUFFER_MSGS 4 /**<Number of messages of size _msgSize to be stored.*/
 #define CIRCULARBUFFER_SIZE CIRCULARBUFFER_MSGS*_msgSize/**< The buffer size.*/
 
 
